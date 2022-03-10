535ad2fadb25430ca9d278a01931b59404b30452
DaniAres <58980062+DaniAres@users.noreply.github.com>
2022-03-10 12:36:39 +0100

    RecUbController

---
+++ apibalrial/src/main/java/org/balrial/apibalrial/controller/ubicacion/RecUbController.java
@@ -0,0 +1,43 @@
+package org.balrial.apibalrial.controller.ubicacion;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.balrial.dao.ubicacion.UbicacionDAO;
+import org.balrial.dao.ubicacion.UbicacionORMDAO;
+import org.balrial.factory.DAOFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Método para recuperar una ubicación
+ */
+
+@RestController
+@RequestMapping("/api")
+public class RecUbController {
+
+    private DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.ORM);
+    private UbicacionDAO ubicacionDAO = factory.getUbicacionDAO();
+
+    @ApiOperation(value = "Endpoint para probar a pasar un valor en el path"
+            , notes = "En este endpoint enviamos un valor en el path de la petición el cual será procesado por la API y nos lo devolverá en modo texto.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "La solicitud ha tenido éxito."),
+            @ApiResponse(code = 400, message = "No se pudo interpretar la solicitud debido a una sintaxis inválida."),
+            @ApiResponse(code = 401, message = "Petició no autorizada, es necesario autenticar para obtener la respuesta solicitada."),
+            @ApiResponse(code = 403, message = "No se poseen los permisos necesarios para la solicitud, por lo que se rechaza la misma."),
+            @ApiResponse(code = 404, message = "El servidor no puede encontrar el contenido solicitado."),
+            @ApiResponse(code = 500, message = "Error inesperado del sistema")})
+
+
+    @GetMapping("/ubicaciones/{idUbicacion}")
+    public void one(@PathVariable int idUbicacion) {
+        UbicacionDAO ubicacionDAO = new UbicacionORMDAO();
+        ubicacionDAO.consultar(idUbicacion);
+
+    }
+
+}
